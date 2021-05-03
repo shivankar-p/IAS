d0acdf20da5759c9e62bed9bb70690c51a664a71
shivankar-p <shivankarpilligundla@gmail.com>
2021-05-03 18:00:23 +0530

    func

--- main.c
+++ main.c
@@ -5,6 +5,46 @@ static int ac[40], mbr[40], mar[12], ibr[20], ir[8], pc;
 
 int ins[2000] = {0}, data[2000] = {0};
 
+void add()
+{
+    //ac = ac+mbr add funcn
+    int cin=0, cout = 0;
+    for(int i = 39; i>=0; i--)
+    {
+        cout = (ac[i]+mbr[i]+cin)/2;                            
+        ac[i] = (ac[i]+mbr[i]+cin)%2;
+        cin = cout;         
+    }
+}
+
+void loadfrmibr()
+{
+    //IBR->IR
+    for(int i = 0; i < 8; i++)
+    {
+        ir[i] = ibr[i];
+    }
+    //ibr-mar
+    for(int i = 8; i < 20; i++)
+    {
+        mar[i-8] = ibr[i];
+    }
+}
+
+void fetch_data()
+{
+    int x = 0;
+    for(int i = 0; i < 12; i++)
+    {
+        x = x*2+mar[i];
+    }
+
+    for(int i = 0; i < 40; i++)
+    {
+        mbr[i] = data[x+i];
+    }
+}
+
 void store()
 {
     int x = 0;
@@ -32,7 +72,6 @@ void load()
     {
         x = x*2+mar[i];
     }
-
     for(int i = 0; i < 40; i++)
     {
         mbr[i] = data[x+i];
@@ -47,23 +86,10 @@ void load()
 
 }
 
-int main()
+void fetch_ins(int prog_c)
 {
-    //initialization
-    //first location
-    ins[7] = 1;
-    ins[25] = 1; ins[27] = 1;
-    ins[34] = 1;
-    ins[36] = 1;
-    //second location
-    ins[62] = 1;
-    ins[67] = 1;
-    ins[73] = 1;
-    ins[75] = 1;
-    //data
-    data[39] = 1; //a = 1
-    data[78] = 1; // b = 2    storing in c
     
+    pc = prog_c;
     //fetch cycle
     //pc->mar
     int tmp_pc = 40*pc;
@@ -85,28 +111,52 @@ int main()
     {
         mbr[i] = ins[adr+i];
     }
-
-    //mbr->ibr
-    for(int i = 20; i < 40; i++)
+    int i;
+    for(i = 0; i < 8; i++)
     {
-        ibr[i-20] = mbr[i];
+        if(mbr[i]!=0) break;
     }
 
-    //mbr->ir
-    for(int i = 0; i < 8; i++)
+    if(i != 8)
     {
-        ir[i] = mbr[i];
-    }
+        //mbr->ibr
+        for(int i = 20; i < 40; i++)
+        {
+            ibr[i-20] = mbr[i];
+        }
 
-    //mbr->mar
-    for(int i = 8; i < 20; i++)
+        //mbr->ir
+        for(int i = 0; i < 8; i++)
+        {
+            ir[i] = mbr[i];
+        }
+
+        //mbr->mar
+        for(int i = 8; i < 20; i++)
+        {
+            mar[i-8] = mbr[i];
+        }
+    }
+    else
     {
-        mar[i-8] = mbr[i];
-    }
-    //fetch cycle ends
+        //mbr->ir
+        for(int i = 0; i < 8; i++)
+        {
+            ir[i] = mbr[20+i];
+        }
 
+        //mbr->mar
+        for(int i = 0; i < 12; i++)
+        {
+            mar[i] = mbr[28+i];
+        }
+    }
     pc += 1;
+    //fetch cycle ends
+}
 
+void decode()
+{
     //decode cycle
     int op = 0;
     for(int i = 0; i < 8; i++)
@@ -117,90 +167,45 @@ int main()
     {
         case 1:
             load();
+            break;
+        case 5:
+            add();
+            break;
+        case 33:
+            store();
+            break;
 
     }
-    //2nd instr
-    //IBR->IR
-    for(int i = 0; i < 8; i++)
-    {
-        ir[i] = ibr[i];
-    }
-    //ibr-mar
-    for(int i = 8; i < 20; i++)
-    {
-        mar[i-8] = ibr[i];
-    }
-    //mem-mbr[i]
-    int x = 0;
-    for(int i = 0; i < 12; i++)
-    {
-        x = x*2+mar[i];
-    }
+}
 
-    for(int i = 0; i < 40; i++)
-    {
-        mbr[i] = data[x+i];
-    }
-    //ac = ac+mbr add funcn
-    int car=0;
-    for(int i = 39; i>=0; i--)
-    {
-        car = (ac[i]+mbr[i]+car)/2;                            
-        ac[i] = (ac[i]+mbr[i]+car)%2;         
-    }
-    //2nd block fetch
-    tmp_pc = 40*pc;
-    for(int i = 11; i >= 0; i--)
-    {
-        mar[i] = tmp_pc%2;
-        tmp_pc /= 2;
-    }
+int main()
+{
+    //initialization
+    //first location
+    ins[7] = 1;
+    ins[25] = 1; ins[27] = 1;
+    ins[34] = 1;
+    ins[36] = 1;
+    //second location
+    ins[62] = 1;
+    ins[67] = 1;
+    ins[73] = 1;
+    ins[75] = 1;
+    //data
+    data[39] = 1; //a = 7
+    data[38] = 1;
+    data[37] = 1;
+    data[79] = 1; // b = 7    storing in c
+    data[78] = 1;
+    data[77] = 1;
 
-    //mar->mem
-    adr = 0;
-    for(int i = 0; i < 12; i++)
-    {
-        adr = adr*2+mar[i];
-    }
-
-    //mem->mbr
-    for(int i = 0; i < 40; i++)
-    {
-        mbr[i] = ins[adr+i];
-    }
-    //checking if left is present
-    int i;
-    for(i = 0; i < 8; i++)
-    {
-        if(mbr[i] != 0) break;
-    }
-    if(i == 8)    //no left inst
-    {
-        //right to ir and mar
-        //mbr->ir
-        for(int i = 0; i < 8; i++)
-        {
-            ir[i] = mbr[20+i];
-        }
-
-        //mbr->mar
-        for(int i = 8; i < 20; i++)
-        {
-            mar[i-8] = mbr[20+i];
-        }
-        //decode
-        op = 0;
-        for(int i = 0; i < 8; i++)
-        {
-            op = op*2+ir[i];
-        }
-        switch(op)
-        {
-            case 33:
-                store();
-        }
-    }
-
+    fetch_ins(pc);
+    decode();
+    loadfrmibr();
+    fetch_data();
+    decode();
+    fetch_ins(pc);
+    decode();
     
     //debug
     for(int i=0; i < 40; i++)
